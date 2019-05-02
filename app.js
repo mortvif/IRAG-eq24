5bd36d5927ff10ba987933609346fff31d4b8f0b
David <mortvif01@gmail.com>
2019-05-02 14:25:45 +0200

    Working system

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const wss = new websocket.Server({
 });
 
 httpServer.listen(PORT, function listening() {
-    console.log('listening on ' + PORT);
+    console.log('HTTP server listening on ' + PORT);
 });
 
 // =============================================================================
@@ -36,61 +36,91 @@ httpServer.listen(PORT, function listening() {
 app.use(cors())
 app.use(express.static('public'));
 app.use("/videos", express.static('videos'));
+app.get("/available-videos", (req, res, next) => {
+    console.log("GET - /available-videos")
+    fs.readdir(__dirname + "/videos", (err, files) => {
+        var videos = [];
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            if(file.split('.').pop() === "mpd"){
+                videos.push(file.split(".")[0])
+            }
+        }
 
+        res.json(videos)
+    })
+})
 // =============================================================================
 // WS Logic
 // =============================================================================
 wss.on("connection", (ws) => {
-    console.log("connection")
+    console.log("WS-New connections")
 
     var name = "video";
-
+    var isFirstmsg = true;
+    var metadata = {};
     ws.on('message', (data) => {
-        console.log("Receiving file");
-        fs.writeFile(__dirname + "/tmp/" + name + ".mp4", data, (err) => {
-            if (err == undefined) {
 
-                var options = [
-                    "-i " + __dirname + "/tmp/" + name + ".mp4",
-                    "-map 0:1",
-                    "-c:a libmp3lame",
-                    "-ar:a:1 22050",
-                    "-c:v libx264",
-                    "-preset ultrafast",
-                    "-keyint_min 120",
-                    "-g 120",
-                    "-use_template 0",
-                    "-media_seg_name '" + name + "_segment-$RepresentationID$-$Number%05d$.m4s'",
-                    "-init_seg_name '" + name + "_initsegment-$RepresentationID$.m4s'",
-                    "-adaptation_sets \"id=0,streams=v id=1,streams=a\""
-                ]
-
-                var n_representations = 5;
-                var bitratres = ["300k", "500k", "1000k", "1500k", "3000k"];
-                var resolutions = ["170x320", "640x480", "720x920", "1080x1920", "1080x1920"];
-
-                for (let i = 0; i < n_representations; i++) {
-                    options.push("-map 0:0");
-                    options.push("-b:v:" + i + " " + bitratres[i]);
-                    options.push("-s:v:" + i + " " + resolutions[i]);
-                }
-
-                options.push("-f dash")
-                options.push(__dirname + "/videos/" + name + ".mpd")
-
-                var cmd = "ffmpeg ";
-                options.forEach((option) => {cmd += (option +" ")})
+        if(isFirstmsg){
+            console.log("WS-Received metadata");
+            isFirstmsg = false;
+            received_metadata = JSON.parse(data);
+            metadata.name = received_metadata.name;
+            metadata.bitrates = received_metadata.bitrates.split(",");
+            metadata.resolutions = received_metadata.resolutions.split(",");
+            console.log(metadata);
+        } else {
+            console.log("WS-Receiving file");
+            fs.writeFile(__dirname + "/tmp/" + metadata.name + ".mp4", data, (err) => {
+                if (err == undefined) {
+    
+                    var options = [
+                        "-i " + __dirname + "/tmp/" + metadata.name + ".mp4",
+                        "-map 0:1",
+                        "-c:a aac",
+                        "-ar:a:1 22050",
+                        "-c:v libx264",
+                        "-preset ultrafast",
+                        "-keyint_min 120",
+                        "-g 120",
+                        "-use_template 0",
+                        "-media_seg_name '" + metadata.name + "_segment-$RepresentationID$-$Number%05d$.m4s'",
+                        "-init_seg_name '" + metadata.name + "_initsegment-$RepresentationID$.m4s'",
+                        "-adaptation_sets \"id=0,streams=v id=1,streams=a\""
+                    ]
+    
+                    var n_representations = metadata.bitrates.length;
+                    var bitratres = metadata.bitrates; //["300k", "500k", "1000k", "1500k", "3000k"];
+                    var resolutions = metadata.resolutions; //["170x320", "640x480", "720x920", "1080x1920", "1080x1920"];
+    
+                    for (let i = 0; i < n_representations; i++) {
+                        options.push("-map 0:0");
+                        options.push("-b:v:" + i + " " + bitratres[i]);
+                        options.push("-s:v:" + i + " " + resolutions[i]);
+                    }
+    
+                    options.push("-f dash")
+                    options.push(__dirname + "/videos/" + metadata.name + ".mpd")
+    
+                    var cmd = "ffmpeg ";
+                    options.forEach((option) => {cmd += (option +" ")})
+                    
+                    var ffmpeg_cmd = exec(cmd, (err,stdout, sdterr) => {
+                        if(err == undefined){
+                            ws.send("Video ready");
+                        } else {
+                            ws.send("Video transcoding failed\n"+stdout)
+                        }
+                        console.log(err)
+                        console.log(stdout)
+                        console.log(sdterr)
+                        console.log("FFMPEG finished");
+                    })
                 
-                var ffmpeg_cmd = exec(cmd, (err,stdout, sdterr) => {
-                    console.log(err)
-                    console.log(stdout)
-                    console.log(sdterr)
-                })
-            
-
-            }
-        })
+    
+                }
+            })
+        }
     })
 })
 
-"id=0,streams=v id=1,streams=a"
--- public/index.js
+++ public/index.js
@@ -1,16 +1,18 @@
 var ws;
-
+var player; 
+var interval;
 function connectWS() {
     ws = new WebSocket("ws://localhost:8080");
     ws.binaryType = "arraybuffer";
 
     ws.onopen = function() {
-        alert("Connected.")
+        alert("Connected.");
+        sendMetadata();
         sendFile();
     };
 
     ws.onmessage = function(evt) {
-        alert(evt.msg);
+        alert(evt.data);
     };
 
     ws.onclose = function() {
@@ -41,6 +43,46 @@ function sendFile() {
     }
 
     reader.readAsArrayBuffer(file);
-
 }
 
+function sendMetadata(){
+    var metadata = {};
+    metadata.resolutions = document.getElementById("resolutions").value;
+    metadata.bitrates = document.getElementById("bitrates").value;
+    metadata.name = document.getElementById("name").value;
+
+    ws.send(JSON.stringify(metadata));
+}
+
+function getAvailable(){
+    var req = new Request(window.location.origin+"/available-videos");
+    fetch(req)
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+        console.log(json)
+        var select = document.getElementById("selected_video");
+        select.innerHTML = "";
+        json.forEach(video => {
+            select.innerHTML+="<option value='"+video+"'>"+video+"</option>"
+        });
+    })
+}
+
+function playVideo(){
+    if (interval != undefined) clearInterval(interval);
+    var selected_video = document.getElementById("selected_video").value;
+    var video = "<video id='video-player' width='720' controls></video>"
+    document.getElementById("video-element").innerHTML=video;
+    document.getElementById("video-element").innerHTML+="<p id='play-info'></p>"
+    var url = window.location.origin+"/videos/"+selected_video+".mpd";
+    player = dashjs.MediaPlayer().create();
+    player.initialize(document.querySelector("#video-player"), url, true);
+    interval = setInterval(() => {
+        var info = JSON.stringify(player.getBitrateInfoListFor("video")[0]);
+        document.getElementById("play-info").innerHTML = info;
+        console.log(info)
+    }, 500)
+
+}
